8cefc0bbbb830ab5a96355ceb3d008f8f500a9f7
CarreyX <2079224160@qq.com>
2026-03-03 00:32:52 +0800

    doc

---
+++ src/main/java/com/haimeixianghao/demo1/controller/SwaggerRedirectController.java
@@ -0,0 +1,16 @@
+package com.haimeixianghao.demo1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+public class SwaggerRedirectController {
+
+    @GetMapping("/swagger")
+    public RedirectView swagger() {
+        return new RedirectView("/swagger-ui/index.html");
+    }
+
+}
+
